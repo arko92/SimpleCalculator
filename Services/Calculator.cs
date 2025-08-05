6a8cd1ed1dbb2d9a77477793f399a4050e35f5f5
arko92 <arkobhattacharjee92@gmail.com>
2025-08-05 19:54:03 +0200

    Add ICalculator interface and Calculator implementation

--- Services/Calculator.cs
+++ Services/Calculator.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SimpleCalculator.Interfaces;
 
 namespace SimpleCalculator.Services
 {
-    internal class Calculator
+    public class Calculator : ICalculator
     {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+
     }
 }
